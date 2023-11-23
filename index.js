d6de6069bd248d453494ba7200e0f3e3f4cefd0b
fai-sas <craftedbyfaisal@gmail.com>
2023-11-23 20:53:49 +0600

    jwt route created

--- index.js
+++ index.js
@@ -27,6 +27,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect()
+
+    // jwt related api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1h',
+      })
+      res.send({ token })
+    })
+
     //  Send a ping to confirm a successful connection
     // await client.db('admin').command({ ping: 1 })
     console.log(
