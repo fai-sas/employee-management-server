940026a326a8e8b8bc85b25a8c6c5d1e34a6c624
fai-sas <craftedbyfaisal@gmail.com>
2023-11-23 20:59:28 +0600

    route created for sign up users

--- index.js
+++ index.js
@@ -52,6 +52,19 @@ async function run() {
       })
     }
 
+    // user related api
+
+    app.post('/users', async (req, res) => {
+      const user = req.body
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query)
+      if (existingUser) {
+        return res.send({ message: 'user already exists', insertedId: null })
+      }
+      const result = await userCollection.insertOne(user)
+      res.send(result)
+    })
+
     //  Send a ping to confirm a successful connection
     // await client.db('admin').command({ ping: 1 })
     console.log(
